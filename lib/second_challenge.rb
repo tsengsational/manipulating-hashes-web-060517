4fc8755243d0c63a9263ada4540d558e253d8578
Jason Tseng <jason.f.tseng@gmail.com>
2017-05-20 05:44:33 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
@@ -7,7 +8,6 @@ def second_challenge
   }
 
   #code your solution here!
-
-  
-
+  groceryList = groceries.values.flatten
+  groceryList
 end
